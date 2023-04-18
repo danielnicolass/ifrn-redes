a0ca4ac680da5213cfdc05db4ec32688e219cb05
Daniel Nicolas Oliveira De Brito <130366087+danielnicolass@users.noreply.github.com>
2023-04-18 17:53:12 -0300

    Update imc.py

--- imc.py
+++ imc.py
@@ -16,3 +16,31 @@ elif imc < 40:
     print("Obesidade")
 else:
     print("Obesidade grave")
+
+    //
+    
+    print("qual o seu peso?")
+peso = float(input())
+print("qual sua altura?")
+altura = float(input())
+imc = peso/altura**2
+print("esse é seu imc:", imc)
+
+if imc< 18.5:
+	print("abaixo do peso")
+
+else:
+    if imc>= 18.5 and imc <= 24.9:
+        print("peso normal")
+    else:
+        if imc>=25 and imc<=  29.9:
+            print("acima do peso")
+        else:
+            if imc>=30 and imc<= 34.9:
+                print("sobrepeso")
+            else:
+                if imc>=35 and imc<= 39.9:
+                    print("obesidade")
+                else:
+                    if imc>40:
+                        print("obesidade grave")
