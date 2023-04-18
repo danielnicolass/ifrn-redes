4b6207a0a212231d7e4a518f7293d9da0b3fb833
Daniel Nicolas Oliveira De Brito <130366087+danielnicolass@users.noreply.github.com>
2023-04-18 16:20:10 -0300

    Atualizar o teste01.py

--- teste01.py
+++ teste01.py
@@ -1,1 +1,12 @@
-print ("ano que vem, vc terá", 2023-int(input("ano de nascimento")) + 1, "anos")
+ano = int(input("Digite um ano: "))
+
+if ano % 400 == 0:
+    print(ano, "é bissexto.")
+else:
+    if ano % 4 == 0:
+        if ano % 100 != 0:
+            print(ano, "é bissexto.")
+        else:
+            print(ano, "não é bissexto.")
+    else:
+        print(ano, "não é bissexto.")
