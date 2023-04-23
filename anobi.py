3949fabb205bf2f39b9fb3dc96309209d29caf4d
Daniel Nicolas Oliveira De Brito <130366087+danielnicolass@users.noreply.github.com>
2023-04-23 16:08:15 -0300

    Atualizar o anobi.py

--- anobi.py
+++ anobi.py
@@ -9,3 +9,22 @@ else:
             print(ano, "não é bissexto.")
     else:
         print(ano, "não é bissexto.")
+        
+        
+        
+        
+ano = int(input("Digite um ano: "))
+
+if ano % 400 == 0:
+    bissexto = True
+elif ano % 100 == 0:
+    bissexto = False
+elif ano % 4 == 0:
+    bissexto = True
+else:
+    bissexto = False
+
+if bissexto:
+    print(f"O ano {ano} é bissexto")
+else:
+    print(f"O ano {ano} não é bissexto")
