29906145dca031d3ba4f365378ec4ce3c3665bc4
Daniel Nicolas Oliveira De Brito <130366087+danielnicolass@users.noreply.github.com>
2023-05-08 14:28:51 -0300

    Atualizar o galileussu.py

--- galileussu.py
+++ galileussu.py
@@ -4,5 +4,14 @@ while x <= 100:
     somaDosQuadrados = somaDosQuadrados + x**2
     x = x+1
 
-print(somaDosQuadrados)
+quadradoDaSoma = 0
+y = 1
+
+while y <= 100:
+  quadradoDaSoma = quadradoDaSoma + y
+  y = y + 1
+
+quadradoDaSoma = quadradoDaSoma**2
+
+print("A diferença da soma dos quadrados dos primeiros cem números naturais pelo o quadrado da soma dos primeiros cem números naturais é: ", quadradoDaSoma - somaDosQuadrados)
 
